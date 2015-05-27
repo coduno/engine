850971b1d5a5118ed1ae568da50520b900aa3245
moritz9422 <moritz9422@gmail.com>
2015-05-27 10:31:36 +0200

    Switched from Python to Go
    Switched from Python to Go in an effort to be consistent with the rest of the
    code base

---
+++ controller/test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"os/exec"
+	"bytes"
+	"io"
+	"log"
+)
+
+var signal = make(chan int)
+
+func send_sig() {
+	signal <- 1
+}
+
+func pipe_output(out io.ReadCloser, dest io.WriteCloser, logBuf *bytes.Buffer) {
+	tempBuf := make([]byte, 1024)
+	writeErr := error(nil)
+	r, readErr := int(0), error(nil)
+	
+	defer out.Close()
+	defer dest.Close()
+	defer send_sig()
+	
+	for ; readErr == nil; {
+		r, readErr = out.Read(tempBuf)
+		logBuf.Write(tempBuf[0:r])
+		
+		if r != 0 && writeErr == nil {
+			_, writeErr := dest.Write(tempBuf[0:r])
+			if writeErr != nil {
+				log.Print(writeErr)
+			}
+		}
+	}
+}
+
+func main() {
+	cmd := exec.Command("timeout", "0.02", "./test.sh")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	var b bytes.Buffer
+	cmd.Start()
+	go pipe_output(stdout, stdin, &b)
+	
+	<-signal
+	log.Print(b.String())
+}
